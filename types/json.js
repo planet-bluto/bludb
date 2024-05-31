8759ccd14c7af77f8fc871635e81356ffc6dbd55
Blu <plush.gamer.456@gmail.com>
2024-05-31 14:18:47 -0400

    Used Task Manager in JSON addon

--- types/json.js
+++ types/json.js
@@ -2,30 +2,38 @@
 const fs = require("fs/promises")
 const fse = require('fs-extra')
 const path = require("path")
+const TaskManager = require("task-manager")
+const Queues = {}
 
 function buildAdapter(root = "db") {
   class JSONAdapter {
     constructor(db_key) {
       this.path = path.join(root, (db_key + ".json"))
+      if (!Object.keys(Queues).includes(db_key)) { Queues[db_key] = new TaskManager() }
+      this.Queue = Queues[db_key]
     }
 
     async read() {
       var data = null
 
-      await fse.ensureFile(this.path)
-      try {
-        data = await fse.readJson(this.path)
-      } catch (err) {
-        // print(err)
-        data = null
-      }
+      await this.Queue.add(async () => {
+        await fse.ensureFile(this.path)
+        try {
+          data = await fse.readJson(this.path)
+        } catch (err) {
+          // print(err)
+          data = null
+        }
+      })
 
       return data
     }
 
     async write(data) {
-      await fse.ensureFile(this.path)
-      await fse.writeJson(this.path, data)
+      await this.Queue.add(async () => {
+        await fse.ensureFile(this.path)
+        await fse.writeJson(this.path, data)
+      })
     }
   }
 
@@ -59,14 +67,19 @@ function buildAdapter(root = "db") {
   }
 
   async function deleter(db_key) {
+    if (!Object.keys(Queues).includes(db_key)) { Queues[db_key] = new TaskManager() }
+    var Queue = Queues[db_key]
+
     var dbFilePath = path.join(root, (db_key + ".json"))
 
-    try {
-      await fs.access(dbFilePath)
-      await fs.unlink(dbFilePath)
-    } catch(err) {
-      // Not real??
-    }
+    await Queue.add(async () => {
+      try {
+        await fs.access(dbFilePath)
+        await fs.unlink(dbFilePath)
+      } catch(err) {
+        // Not real??
+      }
+    })
   }
 
   return [JSONAdapter, getKeys, deleter]
